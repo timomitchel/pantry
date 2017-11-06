bb17caf24deaa3d5e7a19a87e0da2077f4c630a1
Tim Tyrrell <timomitchel23@gmail.com>
2017-11-06 15:56:05 -0700

    final test added

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -282,6 +282,35 @@ class PantryTest < Minitest::Test
     pantry.restock("Raw nuts", 20)
     pantry.restock("Salt", 20)
     expected = [[[20, 10], "Cheese"], [[20, 20], "Flour"], [[10, 40], "Brine"], [[30, 120], "Cucumbers"], [[10, 20], "Raw nuts"], [[10, 20], "Salt"]]
+
     assert_equal expected, pantry.zip_ingredients
+  end
+
+  def test_how_many_can_i_make
+    pantry = Pantry.new
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Cheese", 20)
+    r1.add_ingredient("Flour", 20)
+
+    r2 = Recipe.new("Pickles")
+    r2.add_ingredient("Brine", 10)
+    r2.add_ingredient("Cucumbers", 30)
+
+    r3 = Recipe.new("Peanuts")
+    r3.add_ingredient("Raw nuts", 10)
+    r3.add_ingredient("Salt", 10)
+
+    pantry.add_to_cookbook(r1)
+    pantry.add_to_cookbook(r2)
+    pantry.add_to_cookbook(r3)
+
+    pantry.restock("Cheese", 10)
+    pantry.restock("Flour", 20)
+    pantry.restock("Brine", 40)
+    pantry.restock("Cucumbers", 120)
+    pantry.restock("Raw nuts", 20)
+    pantry.restock("Salt", 20)
+
+    assert_equal ({"Pickles" => 4, "Peanuts" => 2}), pantry.how_many_can_i_make
   end
 end
