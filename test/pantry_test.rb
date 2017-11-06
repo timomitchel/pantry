2376afae05ab35f5d4feb4c4ee50e765c0ba1933
Tim Tyrrell <timomitchel23@gmail.com>
2017-11-06 13:10:32 -0700

    iteration 1 functional with tests

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,33 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def test_pantry_attributes
+    pantry = Pantry.new
+
+    assert_equal ({}), pantry.stock
+  end
+
+  def test_stock_check
+    pantry = Pantry.new
+
+    assert_equal 0, pantry.stock_check('Cheese')
+  end
+
+  def test_restock_changes_stock_check
+    pantry = Pantry.new
+
+
+    pantry.restock("Cheese", 10)
+
+    assert_equal 10, pantry.stock_check('Cheese')
+  end
+
+  def test_restock_adds_to_cheese
+    pantry = Pantry.new
+
+    pantry.restock("Cheese", 10)
+    pantry.restock("Cheese", 20)
+
+    assert_equal 30, pantry.stock_check('Cheese')
+  end
 end
