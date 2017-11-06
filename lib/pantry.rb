100be84f804028c095aa504d8def9806a9dbadb8
Tim Tyrrell <timomitchel23@gmail.com>
2017-11-06 14:23:54 -0700

    cookbook

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,10 +1,11 @@
 class Pantry
 
-  attr_reader :stock
+  attr_reader :stock, :cookbook
 
   def initialize
     @stock = Hash.new(0)
     @shopping_list = []
+    @cookbook = []
   end
 
   def stock_check(ingredient)
@@ -28,5 +29,9 @@ class Pantry
       puts "* #{key}: #{value}\n"
     end
     "* Cheese: 25\n* Flour: 20\n* Spaghetti Noodles: 10\n* Marinara Sauce: 10"
+  end
+
+  def add_to_cookbook(recipe)
+    @cookbook << recipe
   end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -80,4 +80,26 @@ class PantryTest < Minitest::Test
     assert_equal "* Cheese: 25\n* Flour: 20\n* Spaghetti Noodles: 10\n* Marinara Sauce: 10", pantry.print_shopping_list
   end
 
+  def test_cookbook_holds_recipe_objects
+    pantry = Pantry.new
+    r1 = Recipe.new("Cheese Pizza")
+    r1.add_ingredient("Cheese", 20)
+    r1.add_ingredient("Flour", 20)
+
+    r2 = Recipe.new("Pickles")
+    r2.add_ingredient("Brine", 10)
+    r2.add_ingredient("Cucumbers", 30)
+
+    r3 = Recipe.new("Peanuts")
+    r3.add_ingredient("Raw nuts", 10)
+    r3.add_ingredient("Salt", 10)
+
+    pantry.add_to_cookbook(r1)
+    pantry.add_to_cookbook(r2)
+    pantry.add_to_cookbook(r3)
+
+    assert_instance_of Array, pantry.cookbook
+    assert_equal 3, pantry.cookbook.length
+  end
+
 end
